3888f97dfd007b91a1dc8fe9223c879376ff21b8
Jaime Pinheiro <jalepi@live.com>
2021-06-06 01:50:51 +0200

    step two: setup tests

---
+++ src/WebApiExample.Tests/Application.CartonHandler/CartonHandler/AddItems.Tests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebApiExample.Application.CartonHandler
+{
+    public class AddItemsTests
+    {
+        [Fact] public void ItFails() => Assert.True(false);
+    }
+}
